aec1ae398218f36804e4aa67f719600a1c9dcc0e
Artem Korolov <adkorolov@gmail.com>
2026-05-26 18:49:39 +0300

    feat: create StrategyForm component with props

---
+++ src/components/StrategyForm.tsx
@@ -0,0 +1,63 @@
+import React, { use, useState } from "react";
+
+interface StrategyFormProps {
+	onAddStrategy: (
+		coin: string,
+		buyPrice: number,
+		investSum: number,
+		target: number
+	) => void;
+}
+
+export const StrategyForm = ({ onAddStrategy }: StrategyFormProps) => {
+	const [coin, setCoin] = useState('');
+	const [buyPrice, setBuyPrice] = useState('');
+	const [investSum, setInvestSum] = useState('');
+	const [target, setTarget] = useState('5')
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+
+		if (!coin || !buyPrice || !investSum) {
+			return;
+		}
+
+		onAddStrategy(coin, Number(buyPrice), Number(investSum), Number(target));
+		setCoin('');
+		setBuyPrice('');
+		setInvestSum('');
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				placeholder="Coin"
+				value={coin}
+				onChange={(event) => setCoin(event.target.value)}
+			/>
+			<input
+				type="number"
+				placeholder="Buy Price"
+				value={buyPrice}
+				onChange={(event) => setBuyPrice(event.target.value)}
+			/>
+			<input
+				type="number"
+				placeholder="Investment Amount"
+				value={investSum}
+				onChange={(event) => setInvestSum(event.target.value)}
+			/>
+			<input
+				type="number"
+				placeholder="Target (%)"
+				value={target}
+				onChange={(event) => setTarget(event.target.value)}
+			/>
+
+			<button type="submit">Calculate</button>
+		</form>
+	)
+
+
+}
